3230d4a7a12f33baafdb05ee0d79829da08a57cb
Helmut Hummel <typo3@helhum.io>
2024-06-30 18:27:48 +0200

    !!!feat: Rename "variant" argument to "imageVariant" for view helper
    Reasoning: Make sure it can not be confused with "CropVariant"
    
    Also add some basic example in class description and clean up
    argument descriptions.
    
    Resolves: #4

--- Classes/ViewHelpers/ResponsiveImageViewHelper.php
+++ Classes/ViewHelpers/ResponsiveImageViewHelper.php
@@ -8,6 +8,29 @@ use Helhum\TopImage\Rendering\PictureTag;
 use TYPO3\CMS\Core\Resource\FileReference;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
 
+/**
+ * Renders a picture tag for a given image variant identifier.
+ *
+ * Which sources are rendered are defined in configuration.
+ * To be able to respect the different crop areas, the only
+ * possible input to this view helper are FileReference objects.
+ *
+ * This ViewHelper is NOT designed to work in TYPO3 backend!
+ *
+ * Examples
+ * ========
+ *
+ * Default
+ * -------
+ *
+ * ::
+ *
+ *    <ti:responsiveImage image="{imageReference}" variant="news-list" class="class-of-image" />
+ *
+ * Results in the following output within TYPO3 frontend (highly depends on configuration):
+ *
+ * ``<picture><source srcset="/fileadmin/_processed_/a/b/image_asdf.jpg 300w, /fileadmin/_processed_/a/b/image_ghij.jpg 600w" sizes="(min-width: 760px) 50vw, 100vw" media="(max-width: 2048px)" width="300" height="234" /><source srcset="%3$s 1100w" width="1100" height="857" /><img class="class-of-image" src="/fileadmin/_processed_/a/b/image_asdf.jpg" width="300" height="234" alt="alt of image" title="title of image" /></picture>``
+ */
 class ResponsiveImageViewHelper extends AbstractTagBasedViewHelper
 {
     public function __construct(private readonly ImageVariantCollection $imageVariantCollection)
@@ -25,15 +48,15 @@ class ResponsiveImageViewHelper extends AbstractTagBasedViewHelper
             true
         );
         $this->registerArgument(
-            'variant',
+            'imageVariant',
             'string',
-            'image variant to render',
+            'Image variant to render',
             true
         );
         $this->registerArgument(
             'additionalAttributes',
             'array',
-            'additional tag attributes',
+            'Additional tag attributes',
             false,
             []
         );
@@ -42,7 +65,7 @@ class ResponsiveImageViewHelper extends AbstractTagBasedViewHelper
     public function render(): string
     {
         $pictureTag = new PictureTag(
-            imageVariant: $this->imageVariantCollection->get($this->arguments['variant']),
+            imageVariant: $this->imageVariantCollection->get($this->arguments['imageVariant']),
             fileReference: $this->arguments['image'],
             additionalTagAttributes: $this->tag->getAttributes(),
         );
